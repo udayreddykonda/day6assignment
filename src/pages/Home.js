25d727a5861cf818a2542ca239e669c8d563fa5c
udayreddykonda <uday.kumar@terralogic.com>
2020-12-28 12:05:20 +0530

    uday

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import img1 from './images/chicago.jpg';
+
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+            
+            <div className="img1">
+            <img src={img1} alt="chicago" className="img2"/><br />
+           
+
+            </div>
+</div>  
+               
+          
+        );
+    }
+}
+export default home;
---
+++ src/pages/Reports.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Reports() {
+  return (
+    <div className='reports'>
+      <center>
+      <h1>Reports</h1>
+      </center>
+    </div>
+  );
+}
+
+
+export default Reports;
